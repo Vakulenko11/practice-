e5ba741b3e5ebca6aaeadf7af9ba932efe6681c4
Vakulenko11 <fray.2017@inbox.ru>
2017-07-16 19:25:42 +0500

    Add files via upload

---
+++ ZADACHA3/ConsoleApplication3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {          
+                string d = Console.ReadLine();
+                int maxInd = 0;
+                string[] dd = d.Split(' ');
+                int max = dd[0].Length;
+                for (int i = 0; i < dd.Length; i++)
+                    if (max < dd[i].Length) { max = dd[i].Length; maxInd = i; }
+                Console.WriteLine("самое длинное слово: {0} ", dd[maxInd]);
+                Console.WriteLine("индекс самого длинного слова: {0} ", maxInd);
+                Console.WriteLine("длинна: {0} ", dd[maxInd].Length);
+                Console.ReadLine();
+         }
+    }
+}
